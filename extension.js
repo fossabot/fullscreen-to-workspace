661ef1f4e21944bfcdd104a8466ace2f224a1f6d
Matteo Bernardini <mat_teobe@yahoo.it>
2020-03-24 18:04:35 +0100

    fix: go back to old ws when closing fullscreen window - still broken: delete empty workscreen left behind - various other improvements

--- extension.js
+++ extension.js
@@ -1,11 +1,9 @@
 const Meta = imports.gi.Meta;
+const Mainloop = imports.mainloop;
 
-let _handles = {};
-let _previousWorkspace = {};
-let wse;
+let _handles, _previousWorkspace, _wsadd_event, _wsdel_event;
 
-function maximize(act) {
-	const win = act.meta_window;
+function maximize(win) {
 	if (win.window_type !== Meta.WindowType.NORMAL)
 		return;
 	// If the current workspace doesn't have any other windows make it maximized here.
@@ -17,60 +15,79 @@ function maximize(act) {
 	global.screen.get_workspace_by_index(lastworkspace).activate(global.get_current_time());
 }
 
-function unmaximize(act){
-	const win = act.meta_window;
+function unmaximize(win, clean_ws) {
 	if (win.window_type !== Meta.WindowType.NORMAL)
 		return;
 	let previous = _previousWorkspace[win];
 	delete _previousWorkspace[win];
 	if (previous == undefined)
 		return;
-	let old_ws = win.get_workspace();
-	win.change_workspace(previous, true, global.get_current_time());
+	let old_ws = clean_ws || win.get_workspace();
+	global.log(old_ws);
+	if (!clean_ws)
+		win.change_workspace(previous, true, global.get_current_time());
 	previous.activate(global.get_current_time());
 	// Don't leave empty created workspaces behind.
 	if (old_ws.list_windows().filter(w => !w.is_on_all_workspaces()).length == 0)
 		global.screen.remove_workspace(old_ws, global.get_current_time());
 }
 
-function bindNewWindows() {
-	global.get_window_actors().filter((a) => !(a in _handles)).forEach(function(actor) {
-		let resize_event = actor.connect("size-changed", () => {
-			if (actor.meta_window.is_fullscreen())
-				maximize(actor);
-			else
-				unmaximize(actor);
-		});
-		let destroy_event = actor.connect("destroy", () => {
-			// TOFIX: this is not really working
-			actor.disconnect(resize_event);
-			unmaximize(actor);
-			delete _handles[actor];
-		});
-		_handles[actor] = [actor, resize_event, destroy_event];
-	});
+function handleResize(actor) {
+	let win = actor.meta_window;
+	if (win.is_fullscreen())
+		maximize(win);
+	else
+		unmaximize(win);
 }
 
 
 // Mandatory Functions //
 
 function init(extensionMeta) {
-	// Nothing to do here, but this function must exist
+	_handles = {};
+	_previousWorkspace = {};
 }
 
 function enable() {
-	wse = global.screen.connect("window-added", bindNewWindows);
-	// TOFIX: use a better method to bind newly created windows
-	bindNewWindows();
+	// TODO: maybe use a better method to extract parent actor.
+	// the problem is that `window-added`/`window-removed` only give screen and a window
+	// but `size-changed` requires the parent actor, how to get it in a meaningful way?
+	_wsadd_event = global.screen.connect("window-added", (_, win) => {
+		if (win.window_type !== Meta.WindowType.NORMAL)
+			return;
+		// for some reason we need to idle, otherwise the window is not captured
+		// among the global actors yet
+		Mainloop.idle_add(() => {
+			let actor = global.get_window_actors().filter((act) => act.meta_window == win)[0];
+			if (actor in _handles)
+				return;
+			let resize_event = actor.connect("size-changed", handleResize);
+			_handles[actor] = [actor, resize_event];
+		});
+	});
+	_wsdel_event = global.screen.connect("window-removed", (_, win) => {
+		if (win.window_type !== Meta.WindowType.NORMAL)
+			return;
+		let actor = global.get_window_actors().filter((act) => act.meta_window == win)[0];
+		if (!(actor in _handles))
+			return;
+		global.log("detected! "+old_ws);
+		if (win.is_fullscreen())
+			unmaximize(win, true); // TOFIX: how to find the old workspace?
+		delete _handles[actor];
+	});
+	// bind existing windows
+	global.get_window_actors().forEach((actor) => {
+		let resize_event = actor.connect("size-changed", handleResize);
+		_handles[actor] = [actor, resize_event];
+	});
 }
 
 function disable() {
-	for (let actor_s in _handles) {
-		if (_handles.hasOwnProperty(actor_s)) {
-			let [actor, resize_e, destroy_e] = _handles[actor_s];
-			actor.disconnect(resize_e);
-			actor.disconnect(destroy_e);
-		}
-	}
-	global.screen.disconnect(wse);
+	Object.keys(_handles).forEach((actor_s) => {
+		let [actor, resize_e] = _handles[actor_s];
+		actor.disconnect(resize_e);
+	});
+	global.screen.disconnect(_wsadd_event);
+	global.screen.disconnect(_wsdel_event);
 }
